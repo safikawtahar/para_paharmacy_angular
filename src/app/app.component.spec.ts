bfd17ea7c8f823fd3591a373e863bdffe7eab5e3
safikawtahar <kawtthar.safi2000@gmail.com>
2025-01-17 20:25:11 +0100

    jenkins test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'para'`, () => {
+  it(`should have as title 'jenkins-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('para');
+    expect(app.title).toEqual('jenkins-test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('para app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jenkins app test is running!');
   });
 });
